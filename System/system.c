cce67f4847f0405afe3494d7384b841e39c4655a
TaoJun724 <2086239852@qq.com>
2018-10-22 02:44:41 -0700

    system接口

---
+++ System/system.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+  int status = 0;
+  status = system("ps -l");
+  if(status == -1){
+         perror("system()");
+         exit(1);
+  }
+  if(WIFEXITED(status)!=0)//正常退出
+  {
+    if(WEXITSTATUS(status)==0)//操作正确
+      printf("run command success\n");
+    else 
+    {
+       printf("run command fail and exit code is %d\n", WEXITSTATUS(status));
+    }
+  }
+  else 
+  {
+    printf("exit code is  %d\n",  WEXITSTATUS(status));
+  }
+  return 0;
+}
+
