0a0d0ea19028b0f85b3697ca7267ec7689811e1c
TaoJun724 <2086239852@qq.com>
2018-10-25 00:24:37 -0700

    自主shell

--- MyShell/myshell.c
+++ MyShell/myshell.c
@@ -3,6 +3,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<ctype.h>
 
 
 #define MAX 1024
@@ -24,11 +28,61 @@ void do_parse(char *cmd)//解析命令行
 void do_execute()
 {
   pid_t id =fork();
-  if(id == 0){
+  if(id == 0)
+  {
+    int i = 0;
+    int flag = 0;
+    //判断命令是否含有输入输出重定向
+    for(;myargv[i] !=  NULL;i++)
+    {
+      //输出重定向
+      if(strcmp(myargv[i],">") == 0){
+        flag = 1;
+        break;
+      }
+      //输入重定向
+      
+      if(strcmp(myargv[i],"<") == 0){
+        flag = 2;
+        break;
+      }
+    }
+    //处理输出重定向
+      if(flag == 1)
+      {
+        myargv[i] = NULL;
+        int oldfd = open(myargv[i+ 1],O_WRONLY|O_CREAT,0777);
+        if(oldfd < 0){
+          perror("open");
+          exit(1);
+        }
+        close(1);
+        int newfd = dup(oldfd);   
+      }
+  
+
+    //输入重定向
+      if(flag == 2)
+      {
+        myargv[i] = NULL;
+        int oldfd = open(myargv[i+ 1],O_RDONLY,0644);
+        if(oldfd < 0){
+          perror("open");
+          exit(1);
+        }
+        close(0);
+        int newfd = dup(oldfd);        
+      }
+    
+
     execvp(myargv[0],myargv);
     exit(1);
   }
-  else{
+
+
+
+  if(id > 0)
+  {
     waitpid(id,NULL,0);
   }
 }
@@ -46,3 +100,26 @@ int main()
   }
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
