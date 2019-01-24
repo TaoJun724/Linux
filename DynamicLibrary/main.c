dddd40c3dc75ae3d301cdf5f098761797da143f0
TaoJun724 <2086239852@qq.com>
2019-01-23 23:32:13 -0800

    dynamic library test

---
+++ DynamicLibrary/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include"add.h"
+#include"sub.h"
+
+int main()
+{
+  printf("5+3=%d\n",add(5,3));
+  printf("8-2=%d\n",sub(8,2));
+}
---
+++ DynamicLibrary/sub.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"sub.h"
+
+int sub(int x,int y)
+{
+  return x-y;
+}
+
+
+
